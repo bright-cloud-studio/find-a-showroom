7b819f6f29568923a15d5f9fb0ff4d65d01cc4e0
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-08-08 15:27:51 -0400

    Create tl_showroom.php

---
+++ system/modules/frasch_find_a_showroom/dca/tl_showroom.php
@@ -0,0 +1,240 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Find A Showroom
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/frasch-find-a-showroom
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+ 
+/* Table tl_showroom */
+$GLOBALS['TL_DCA']['tl_showroom'] = array
+(
+ 
+    // Config
+    'config' => array
+    (
+        'dataContainer'               => 'Table',
+        'enableVersioning'            => true,
+        'sql' => array
+        (
+            'keys' => array
+            (
+                'id' 	=> 	'primary',
+                'rep_name' =>  'index'
+            )
+        )
+    ),
+ 
+    // List
+    'list' => array
+    (
+        'sorting' => array
+        (
+            'mode'                    => 1,
+            'fields'                  => array('name'),
+            'flag'                    => 1,
+            'panelLayout'             => 'filter;search,limit'
+        ),
+        'label' => array
+        (
+            'fields'                  => array('state', 'city', 'name'),
+            'format'                  => '%s - %s - %s'
+        ),
+        'global_operations' => array
+        (
+            'export' => array
+            (
+                'label'               => 'Export Showrooms CSV',
+                'href'                => 'key=exportShowrooms',
+                'icon'                => 'system/modules/frasch_find_a_showroom/assets/icons/file-export-icon-16.png'
+            ),
+            'all' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['MSC']['all'],
+                'href'                => 'act=select',
+                'class'               => 'header_edit_all',
+                'attributes'          => 'onclick="Backend.getScrollOffset()" accesskey="e"'
+            )
+
+        ),
+        'operations' => array
+        (
+            'edit' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_showroom']['edit'],
+                'href'                => 'act=edit',
+                'icon'                => 'edit.gif'
+            ),
+			
+            'copy' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_showroom']['copy'],
+                'href'                => 'act=copy',
+                'icon'                => 'copy.gif'
+            ),
+            'delete' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_showroom']['delete'],
+                'href'                => 'act=delete',
+                'icon'                => 'delete.gif',
+                'attributes'          => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
+            ),
+            'toggle' => array
+            (
+              'label'               => &$GLOBALS['TL_LANG']['tl_showroom']['toggle'],
+              'icon'                => 'visible.gif',
+              'attributes'          => 'onclick="Backend.getScrollOffset();return AjaxRequest.toggleVisibility(this,%s)"',
+              'button_callback'     => array('Bcs\Backend\Showrooms', 'toggleIcon')
+            ),
+            'show' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_showroom']['show'],
+                'href'                => 'act=show',
+                'icon'                => 'show.gif'
+            )
+        )
+    ),
+ 
+    // Palettes
+    'palettes' => array
+    (
+        'default'                     => '{rep_legend},rep_name,company_name,region,product_line,address,city,zip,phone_number,alt_phone_number,email,website;{state_legend},state;{publish_legend},published;'
+    ),
+ 
+    // Fields
+    'fields' => array
+    (
+        'id' => array
+        (
+            'sql'                     => "int(10) unsigned NOT NULL auto_increment"
+        ),
+        'tstamp' => array
+        (
+            'sql'                     => "int(10) unsigned NOT NULL default '0'"
+        ),
+    		'sorting' => array
+    		(
+            'sql'                     => "int(10) unsigned NOT NULL default '0'"
+    		),
+    		'name' => array
+    		(
+    			'label'                   => &$GLOBALS['TL_LANG']['tl_rep']['name'],
+    			'inputType'               => 'text',
+    			'default'                 => '',
+    			'search'                  => true,
+    			'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+    			'sql'                     => "varchar(255) NOT NULL default ''"
+    		),
+        'territory_sales_manager' => array
+    		(
+    			'label'                   => &$GLOBALS['TL_LANG']['tl_rep']['territory_sales_manager'],
+    			'inputType'               => 'text',
+    			'default'                 => '',
+    			'search'                  => true,
+    			'eval'                    => array('mandatory'=>false, 'tl_class'=>'w50'),
+    			'sql'                     => "varchar(255) NOT NULL default ''"
+    		),
+        'street_address' => array
+    		(
+    			'label'                   => &$GLOBALS['TL_LANG']['tl_rep']['street_address'],
+    			'inputType'               => 'text',
+    			'default'                 => '',
+    			'search'                  => true,
+    			'eval'                    => array('mandatory'=>false, 'tl_class'=>'w50'),
+    			'sql'                     => "varchar(255) NOT NULL default ''"
+    		),
+        'product_line' => array
+    		(
+    			'label'                   => &$GLOBALS['TL_LANG']['tl_rep']['product_line'],
+    			'inputType'               => 'select',
+                'options'                 => array('' => ' ', 'full_line' => 'Full Line', 'lighting_only' => 'Lighting Only', 'corporate' => 'Corporate'),
+                'eval'                    => array('mandatory'=>false, 'tl_class'=>'w50'),
+                'sql'                     => "varchar(32) NOT NULL default ''"
+    		),
+        'address' => array
+    		(
+    			'label'                   => &$GLOBALS['TL_LANG']['tl_rep']['address'],
+    			'inputType'               => 'text',
+    			'default'                 => '',
+    			'search'                  => true,
+    			'eval'                    => array('mandatory'=>false, 'tl_class'=>'w50'),
+    			'sql'                     => "varchar(255) NOT NULL default ''"
+    		),
+        'city' => array
+    		(
+    			'label'                   => &$GLOBALS['TL_LANG']['tl_rep']['city'],
+    			'inputType'               => 'text',
+    			'default'                 => '',
+    			'search'                  => true,
+    			'eval'                    => array('mandatory'=>false, 'tl_class'=>'w50'),
+    			'sql'                     => "varchar(255) NOT NULL default ''"
+    		),
+        'state' => array
+    		(
+    			'label'                   => &$GLOBALS['TL_LANG']['tl_rep']['state'],
+    			'inputType'               => 'checkbox',
+    			'default'				  => '',
+    			'options_callback'		  => array('Bcs\Backend\Reps', 'getStates'),
+    			'eval'                    => array('multiple'=>true, 'chosen'=>true, 'tl_class'=>'w50'),
+    			'sql'                     => "varchar(255) NOT NULL default ''"
+    		),
+        'zip' => array
+    		(
+    			'label'                   => &$GLOBALS['TL_LANG']['tl_rep']['zip'],
+    			'inputType'               => 'text',
+    			'default'                 => '',
+    			'search'                  => true,
+    			'eval'                    => array('mandatory'=>false, 'tl_class'=>'w50'),
+    			'sql'                     => "varchar(255) NOT NULL default ''"
+    		),
+        'phone_number' => array
+    		(
+    			'label'                   => &$GLOBALS['TL_LANG']['tl_rep']['phone_number'],
+    			'inputType'               => 'text',
+    			'default'                 => '',
+    			'search'                  => true,
+    			'eval'                    => array('mandatory'=>false, 'tl_class'=>'w50'),
+    			'sql'                     => "varchar(255) NOT NULL default ''"
+    		),
+        'alt_phone_number' => array
+    		(
+    			'label'                   => &$GLOBALS['TL_LANG']['tl_rep']['alt_phone_number'],
+    			'inputType'               => 'text',
+    			'default'                 => '',
+    			'search'                  => true,
+    			'eval'                    => array('mandatory'=>false, 'tl_class'=>'w50'),
+    			'sql'                     => "varchar(255) NOT NULL default ''"
+    		),
+        'email' => array
+    		(
+    			'label'                   => &$GLOBALS['TL_LANG']['tl_rep']['email'],
+    			'inputType'               => 'text',
+    			'default'                 => '',
+    			'search'                  => true,
+    			'eval'                    => array('mandatory'=>false, 'tl_class'=>'w50'),
+    			'sql'                     => "varchar(255) NOT NULL default ''"
+    		),
+        'website' => array
+    		(
+    			'label'                   => &$GLOBALS['TL_LANG']['tl_rep']['website'],
+    			'inputType'               => 'text',
+    			'default'                 => '',
+    			'search'                  => true,
+    			'eval'                    => array('mandatory'=>false, 'tl_class'=>'w50'),
+    			'sql'                     => "varchar(255) NOT NULL default ''"
+    		),
+    		'published' => array
+    		(
+    			'exclude'                 => true,
+    			'label'                   => &$GLOBALS['TL_LANG']['tl_rep']['published'],
+    			'inputType'               => 'checkbox',
+    			'eval'                    => array('submitOnChange'=>true, 'doNotCopy'=>true),
+    			'sql'                     => "char(1) NOT NULL default ''"
+    		)		
+    )
+);
