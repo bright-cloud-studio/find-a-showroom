aaf0a627fc34f344e52f3dfb211275110eff3a0c
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-08-08 15:34:51 -0400

    Create tl_showroom.php

---
+++ system/modules/frasch_find_a_showroom/languages/en/tl_showroom.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Find A Showroom
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/frasch-find-a-showroom
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+/* Legends */
+$GLOBALS['TL_LANG']['tl_showroom']['rep_legend'] 	= 'Showroom Details';
+$GLOBALS['TL_LANG']['tl_showroom']['state_legend'] 	= 'Select States';
+$GLOBALS['TL_LANG']['tl_showroom']['publish_legend'] 	= 'Publish';
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_showroom']['showroom_name']            = array('Showroom Name', 'Enter the showroom\'s name.');
+$GLOBALS['TL_LANG']['tl_showroom']['territory_sales_manager']  = array('Territory Sales Manager', 'Enter the territory sales manager');
+$GLOBALS['TL_LANG']['tl_showroom']['street_address']           = array('Street Address', 'Enter the street address');
+$GLOBALS['TL_LANG']['tl_showroom']['city']                     = array('City', 'Select the city');
+$GLOBALS['TL_LANG']['tl_showroom']['state']                    = array('State', 'Enter the state');
+$GLOBALS['TL_LANG']['tl_showroom']['products']                 = array('Products', 'Enter the products');
+$GLOBALS['TL_LANG']['tl_showroom']['partner_type']             = array('Partner Type', 'Enter the partner type');
+$GLOBALS['TL_LANG']['tl_showroom']['gallery_url']              = array('Gallery URL', 'Enter the gallery url');
+ 
+ /* Buttons */
+$GLOBALS['TL_LANG']['tl_showroom']['new']     = array('New Showroom', 'Add a new record');
+$GLOBALS['TL_LANG']['tl_showroom']['show']  	= array('Showroom details', 'Show the details of record ID %s');
+$GLOBALS['TL_LANG']['tl_showroom']['edit']  	= array('Edit Showroom', 'Edit record ID %s');
+$GLOBALS['TL_LANG']['tl_showroom']['copy']  	= array('Copy Showroom', 'Copy record ID %s');
+$GLOBALS['TL_LANG']['tl_showroom']['delete']  = array('Delete Showroom', 'Delete record ID %s');
+$GLOBALS['TL_LANG']['tl_showroom']['toggle'] 	= array('Toggle Showroom', 'Toggle record ID %s');
