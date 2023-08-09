9e6f375c511ffd0462a0b052a38dd025f416be91
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-08-09 10:49:28 -0400

    Update tl_showroom.php

--- system/modules/frasch_find_a_showroom/dca/tl_showroom.php
+++ system/modules/frasch_find_a_showroom/dca/tl_showroom.php
@@ -170,7 +170,7 @@ $GLOBALS['TL_DCA']['tl_showroom'] = array
     		(
     			'label'                   => &$GLOBALS['TL_LANG']['tl_showroom']['products'],
     			'inputType'               => 'select',
-                'options'                 => array('' => ' ', 'full_line' => 'Full Line', 'lighting_only' => 'Lighting Only', 'corporate' => 'Corporate'),
+                'options'                 => array('' => ' ', 'bafl' => 'BAFL', 'brik' => 'BRIK', 'brik_1_0' => 'BRIK 1.0', 'cloud' => 'Cloud', 'linyfelt' => 'Linyfelt', 'multi' => 'Multi', 'plank' => 'Plank'),
                 'eval'                    => array('mandatory'=>false, 'tl_class'=>'w50'),
                 'sql'                     => "varchar(32) NOT NULL default ''"
     		),
