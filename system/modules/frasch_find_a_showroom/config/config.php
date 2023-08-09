75ee6060137071e7be80cf5c1903f9dcb7b638ce
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-08-09 08:46:58 -0400

    Create config.php

---
+++ system/modules/frasch_find_a_showroom/config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Find Your Rep
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/frasch-find-your-rep
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+/* Back End Modules */
+$GLOBALS['BE_MOD']['content']['showrooms'] = array(
+    'tables'=> array('tl_showroom')
+};
