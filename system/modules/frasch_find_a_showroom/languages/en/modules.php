281ea5520c9401c2e0ba964e87cb4b4c7c7a2f41
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-08-09 09:40:15 -0400

    Create modules.php

---
+++ system/modules/frasch_find_a_showroom/languages/en/modules.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Find A Showroom
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/frasch-find-a-showroom
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+/* Front end modules */
+$GLOBALS['TL_LANG']['FMD']['mod_find_a_showroom'] 	= array('Showrooms Module', 'Module that displays our Showroom Finder on a page.');
+
+/* Back end modules */
+$GLOBALS['TL_LANG']['MOD']['showrooms'] = array('Showrooms', 'Configure Showrooms');
