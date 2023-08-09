cc6342688df27a92cf455934a870efbc1e2b262d
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-08-09 10:19:11 -0400

    Update Showroom.php

--- system/modules/frasch_find_a_showroom/library/Bcs/Model/Showroom.php
+++ system/modules/frasch_find_a_showroom/library/Bcs/Model/Showroom.php
@@ -1,19 +1,19 @@
 <?php
 
 /**
- * Bright Cloud Studio's Find Your Rep
+ * Bright Cloud Studio's Find A Showroom
  *
  * Copyright (C) 2023 Bright Cloud Studio
  *
- * @package    bright-cloud-studio/frasch-find-your-rep
+ * @package    bright-cloud-studio/frasch-find-a-showroom
  * @link       https://www.brightcloudstudio.com/
  * @license    http://opensource.org/licenses/lgpl-3.0.html
  */
 
 namespace Bcs\Model;
 
-class Rep extends \Model
+class Showroom extends \Model
 {
 	/* Table name */
-	protected static $strTable = 'tl_rep';	
+	protected static $strTable = 'tl_showroom';	
 }
